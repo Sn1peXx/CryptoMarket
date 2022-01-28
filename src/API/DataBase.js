d493b9b6ee08ef33cea363f8d7b3e7bfacc57049
Andrey Lavrusenko <andreylavrusenko@MacBook-Air-Andrey.local>
2022-01-28 22:05:38 +0300

    fix login

--- src/API/DataBase.js
+++ src/API/DataBase.js
@@ -3,87 +3,102 @@ import {getDatabase, onValue, ref, remove, set, update} from "firebase/database"
 export const DataBase = {
 
     getOrderHistory() {
-        const db = getDatabase();
-        const userId = JSON.parse(localStorage.getItem("user"))[2];
+        try {
+            const db = getDatabase();
+            const userId = JSON.parse(localStorage.getItem("user"))[2];
 
-        const starCountRef = ref(db, 'OrderHistory/' + userId);
-        onValue(starCountRef, (snapshot) => {
-            try {
-                window.order = Object.values(snapshot.val());
-            } catch (e) {
-            }
-        });
+            const starCountRef = ref(db, 'OrderHistory/' + userId);
+            onValue(starCountRef, (snapshot) => {
+                try {
+                    window.order = Object.values(snapshot.val());
+                } catch (e) {
+                }
+            });
+        } catch (e) {}
+
     },
 
     getBalance() {
-        const db = getDatabase();
-        const userId = JSON.parse(localStorage.getItem("user"))[2];
+       try {
+           const db = getDatabase();
+           const userId = JSON.parse(localStorage.getItem("user"))[2];
 
-        const starCountRef2 = ref(db, 'Balance/' + userId);
-        onValue(starCountRef2, (snapshot) => {
-            try {
-                let res = Object.values(snapshot.val())
-                window.balance = res[0];
-            } catch (e) {
-            }
+           const starCountRef2 = ref(db, 'Balance/' + userId);
+           onValue(starCountRef2, (snapshot) => {
+               try {
+                   let res = Object.values(snapshot.val())
+                   window.balance = res[0];
+               } catch (e) {
+               }
 
-        });
+           });
+       } catch (e) {}
     },
 
     getNewDeal() {
-        const db = getDatabase();
-        const userId = JSON.parse(localStorage.getItem("user"))[2];
+        try {
+            const db = getDatabase();
+            const userId = JSON.parse(localStorage.getItem("user"))[2];
 
-        const starCountRef3 = ref(db, 'NewDeal/' + userId);
-        onValue(starCountRef3, (snapshot) => {
-            try {
-                window.deal = Object.values(snapshot.val());
-            } catch (e) {
-            }
-        });
+            const starCountRef3 = ref(db, 'NewDeal/' + userId);
+            onValue(starCountRef3, (snapshot) => {
+                try {
+                    window.deal = Object.values(snapshot.val());
+                } catch (e) {
+                }
+            });
+        } catch (e) {}
     },
 
     setOrderHistory(id, coin, orderValue, dealVolume, dealDate) {
-        const db = getDatabase();
-        const userId = JSON.parse(localStorage.getItem("user"))[2];
+        try {
+            const db = getDatabase();
+            const userId = JSON.parse(localStorage.getItem("user"))[2];
 
-        set(ref(db, 'OrderHistory/' + userId + `/${id}`), {
-            id,
-            coin,
-            orderValue,
-            dealVolume,
-            dealDate
-        }).catch(() => {
-            alert('Ошибка')
-        })
+            set(ref(db, 'OrderHistory/' + userId + `/${id}`), {
+                id,
+                coin,
+                orderValue,
+                dealVolume,
+                dealDate
+            }).catch(() => {
+                alert('Ошибка')
+            })
+        } catch (e) {}
     },
 
     setNewDeal(id, coin, dealVolume) {
-        const db = getDatabase();
-        const userId = JSON.parse(localStorage.getItem("user"))[2];
+        try {
+            const db = getDatabase();
+            const userId = JSON.parse(localStorage.getItem("user"))[2];
 
-        set(ref(db, 'NewDeal/' + userId + `/${id}`), {
-            id,
-            coin,
-            dealVolume
-        }).catch(() => {
-            alert('Ошибка')
-        })
+            set(ref(db, 'NewDeal/' + userId + `/${id}`), {
+                id,
+                coin,
+                dealVolume
+            }).catch(() => {
+                alert('Ошибка')
+            })
+        } catch (e) {}
     },
 
     updateBalance(newBalance) {
-        const db = getDatabase();
-        const userId = JSON.parse(localStorage.getItem("user"))[2];
+       try {
+           const db = getDatabase();
+           const userId = JSON.parse(localStorage.getItem("user"))[2];
 
-        update(ref(db, 'Balance/' + userId), {
-            balance: newBalance
-        })
+           update(ref(db, 'Balance/' + userId), {
+               balance: newBalance
+           })
+       } catch (e) {}
     },
 
     closeDeal(id) {
-        const db = getDatabase();
-        const userId = JSON.parse(localStorage.getItem("user"))[2];
+        try {
+            const db = getDatabase();
+            const userId = JSON.parse(localStorage.getItem("user"))[2];
 
-        remove(ref(db, 'NewDeal/' + userId + `/${id}`))
+            remove(ref(db, 'NewDeal/' + userId + `/${id}`))
+        } catch (e) {}
     }
 }
